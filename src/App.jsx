2db7b9ab0dabba3edc7f770fda883db0b3c8d71b
Ramandeep Singh <ramandeepsingh40270@gmail.com>
2024-05-23 16:31:34 +0530

    Changes in the UI

--- src/App.jsx
+++ src/App.jsx
@@ -11,13 +11,13 @@ const App = () => {
 
   return (
     <div className="">
-      <div className="bg-[rgba(47,47,47,255)] flex text-base h-12 text-[#9b9b9a]">
+      <div className="bg-[rgba(47,47,47,255)] flex text-base h-8 text-[#9b9b9a] hover:z-100 hover:bg-[rgba(65,65,65,255)]">
         <div
           onClick={() => clickHandler(1)}
-          className={` absolute pt-3  left-0 h-12  w-[154px] text-center ${
+          className={` absolute pt-1  left-0 h-8  w-[154px] text-center ${
             active === 1
               ? "bg-[rgba(33,33,33,255)] rounded-t-2xl border-l-2 border-t-2 border-[rgba(47,47,47,255)]"
-              : "bg-[rgba(47,47,47,255)] z-10 rounded-br-2xl hover:cursor-pointer"
+              : "bg-[rgba(47,47,47,255)] z-10 rounded-br-2xl hover:cursor-pointer hover:z-30 hover:bg-[rgba(65,65,65,255)]"
           }`}
         >
           Stats
@@ -37,10 +37,10 @@ const App = () => {
 
         <div
           onClick={() => clickHandler(2)}
-          className={` pt-3 absolute h-12 px-5 right-0 w-[158px] text-center ${
+          className={` pt-1 absolute h-8 px-5 right-0 w-[158px] text-center ${
             active === 2
               ? "bg-[rgba(33,33,33,255)] rounded-t-2xl border-t-2 border-r-2 border-[rgba(47,47,47,255)]"
-              : "bg-[rgba(47,47,47,255)] z-10 rounded-bl-2xl hover:cursor-pointer"
+              : "bg-[rgba(47,47,47,255)] z-10 rounded-bl-2xl hover:cursor-pointer hover:z-30 hover:bg-[rgba(65,65,65,255)]"
           }`}
         >
           Block
